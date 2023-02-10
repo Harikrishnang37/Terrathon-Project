4e106abf09532e1b08e40e1e12fa3b66369cfbeb
Varsha Rohidekar <varsharohidekar@Varshas-MacBook-Pro.local>
2023-02-11 02:07:10 +0530

    created react app

---
+++ react app/app1/src/screens/Login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Login.css'
+import axios from "axios";
+export default function Login() {
+  return (
+    <div>
+       <title>Login</title>
+          <link rel="stylesheet" type="text/css" href="Login.css" />
+          <div className="loginbox">
+            <img src="avtaar.jpg" className="avtaar" />
+            <h1>Login Here</h1>
+            <form>
+              <p>Username</p>
+              <input type="text" name placeholder="Enter Username" />
+              <p>Password</p>
+              <input type="password" name placeholder="Enter Password" />
+              <input type="submit" name defaultValue="Login" />
+              <a href="#">Don't Have an Account</a>
+            </form>
+          </div>
+    </div>
+  )
+}
+
+function test(){
+
+axios.post('/products/add', {username:"user1",userid: 1,productname: "prod1", productid: 1, price: 250, rating: 3,ImgURL: "hello.com"});
+}
