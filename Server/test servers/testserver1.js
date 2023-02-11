eb6f72680a67f13ac429a8786beb0d00032f76d5
Hari <harikrishnang37@gmail.com>
2023-02-11 05:51:22 +0530

    fixed user auth

--- Server/test servers/testserver1.js
+++ Server/test servers/testserver1.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const mng = require("mongoose");
 const Products = require("./Products");
+const Users = require("./Users");
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 const connection_url = "mongodb+srv://harikrishnan:fordb@cluster0.mkn4pum.mongodb.net/Cluster0?retryWrites=true&w=majority"
 
@@ -30,7 +31,23 @@ app.post("/products/add", (req,res) => {
 })
 
 app.post("/users/add", (req,res) => {
-    // to be completed
+    const UserDetail = req.body;
+
+    console.log("User detail:", UserDetail);
+
+    Users.create(UserDetail, (err, data) =>{
+        if(err){
+            res.status(500).send(err.message);
+        }
+        else
+            res.status(201).send(data);
+    })
+})
+
+app.get("/users/verify", (req,res) => {
+    
+    Users.find().then(prod => res.json(prod)).catch(err =>res.status(400).json("Error"+err))
+    //console.log(Products.find());
 })
 
 app.listen(port, ()=> console.log("listening on port ", port));
--- react app/app1/src/screens/Login.js
+++ react app/app1/src/screens/Login.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react';
 import './Login.css'
 import axios from "axios";
+//import useNavigate from 'react-router-dom';
+
 export default function Login() {
+
+  //const navigate = useNavigate()
+    const [username,setUser]=useState("")
+    const [password,setPassword]=useState("")
+
+    async function handleSubmit(event)
+    {   
+        console.log("submit clicked")
+        event.preventDefault()
+
+        console.log("username:"  +username);
+        console.log("password:"  +password);
+        if(username && password)
+        {   
+             const user_data={name:username,password:password}
+             const pdata= await axios.get("http://localhost:8000/users/verify");
+             const data = pdata.data;
+
+             console.log(data)
+             
+
+             if(data===null){
+                 alert("alert- no such user")
+             }
+
+             var flag = false;
+             for (var i in data){
+              //console.log("this user: "+ i)
+              if(data[i].username == user_data.name && data[i].password == user_data.password){
+                //navigate
+                console.log("log in successful");
+                flag = true;
+              }
+             }
+
+             if(flag == false) alert("unsuccessful attempt");
+         }
+            
+        }
+    
+
+    function handleUser(event)
+    {
+        setUser(event.target.value)
+       // console.log(event.target.value)
+    }
+
+    function handlePassword(event)
+    {
+        setPassword(event.target.value)
+        // console.log(event.target.value)
+    }
+
   return (
     <div>
        <title>Login</title>
@@ -9,12 +64,12 @@ export default function Login() {
           <div className="loginbox">
             <img src="avtaar.jpg" className="avtaar" />
             <h1>Login Here</h1>
-            <form >
+            <form onSubmit={ handleSubmit}>
               <p>Username</p>
-              <input type="text" name placeholder="Enter Username" />
+              <input type="text" name placeholder="Enter Username" value = {username} onChange = {handleUser}/>
               <p>Password</p>
-              <input type="password" name placeholder="Enter Password" />
-              <input type="button" name defaultValue="Login" onClick={test}/>
+              <input type="password" name placeholder="Enter Password" value = {password} onChange = {handlePassword} />
+              <input type="submit" name defaultValue="Login"/>
               <a href="#">Don't Have an Account</a>
             </form>
           </div>
@@ -22,9 +77,14 @@ export default function Login() {
   )
 }
 
-function test(){
+async function test(){
 
   console.log("hello");
 
-axios.post('/products/add', {username:"user1",userid: 1,productname: "prod1", productid: 1, price: 250, rating: 3,ImgURL: "hello.com"});
+ axios.post('http://localhost:8000/users/add', {username:"user7" , password: "pwd"});
+
+// const data = await axios.get("http://localhost:8000/users/verify");
+// console.log(data);
+// console.log("haiii");
+
 }
